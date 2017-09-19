3c4d0a8a333ee7230931d27890d957262e54b902
virattt <virat.dot@gmail.com>
2017-09-18 23:21:08 -0700

    Removed Block object

--- src/main/scala/models/Block.scala
+++ src/main/scala/models/Block.scala
@@ -3,18 +3,23 @@ package models
 import java.math.BigInteger
 import java.security.MessageDigest
 
-class Block(index: Long, timestamp: Long, data: Data, previousHash: String, hash: String) {
+/**
+ * @param index - the index of the Block in the Blockchain
+ * @param timestamp - the timestamp in seconds, since January 1, 1970
+ * @param data - the Data that this Block contains
+ * @param previousHash - the Hash of the previous Block in the Blockchain
+ */
+class Block(index: Long, timestamp: Long, data: Data, previousHash: String) {
 
-  def hashcode: Int = hash.hashCode()
-}
+  def hash: String = hash(index, timestamp, data, previousHash)
 
-object Block {
-
-  def calculateHash(index: Long, timestamp: Long, data: Data, previousHash: String): String = {
-    sha256(index.toString + timestamp.toString + data.toString + previousHash.toString)
+  private def hash(index: Long, timestamp: Long, data: Data, previousHash: String): String = {
+    val input = index.toString + timestamp.toString + data.toString + previousHash.toString
+    sha256(input)
   }
 
-  private def sha256(text: String) : String = String.format("%064x",
-    new BigInteger(1, MessageDigest.getInstance("SHA-256").digest(text.getBytes("UTF-8")))
-  )
+  private def sha256(text: String) : String = {
+    val message: Array[Byte] = MessageDigest.getInstance("SHA-256").digest(text.getBytes("UTF-8"))
+    String.format("%064x", new BigInteger(1, message))
+  }
 }
